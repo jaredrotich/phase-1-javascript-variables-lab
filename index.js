c48511cc9984bcaf89ba47b55f182daccf6b0b64
jared <jaredrotich3@gmail.com>
2025-03-13 15:08:48 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,103 @@
-// Code your solution in this file!
+
+{
+    "stats"; {
+      "suites"; 4,
+      "tests"; 6,
+      "passes"; 6,
+      "pending"; 0,
+      "failures"; 0,
+      "start"; "2025-03-06T19:58:00.726Z",
+      "end"; "2025-03-06T19:58:01.552Z",
+      "duration"; 826
+    }
+    "tests"; [
+      {
+        "title": "is set as Scuber",
+        "fullTitle": "index.js companyName is set as Scuber",
+        "duration": 1,
+        "currentRetry": 0,
+        "err": {}
+      },
+      {
+        "title": "is defined as a const",
+        "fullTitle": "index.js companyName is defined as a const",
+        "duration": 1,
+        "currentRetry": 0,
+        "err": {}
+      },
+      {
+        "title": "is declared as equal to Chelsea",
+        "fullTitle": "index.js mostProfitableNeighborhood is declared as equal to Chelsea",
+        "duration": 0,
+        "currentRetry": 0,
+        "err": {}
+      },
+      {
+        "title": "is defined using let",
+        "fullTitle": "index.js mostProfitableNeighborhood is defined using let",
+        "duration": 0,
+        "currentRetry": 0,
+        "err": {}
+      },
+      {
+        "title": "is declared as equal to Susan Smith",
+        "fullTitle": "index.js companyCeo is declared as equal to Susan Smith",
+        "duration": 0,
+        "currentRetry": 0,
+        "err": {}
+      },
+      {
+        "title": "is defined using let",
+        "fullTitle": "index.js companyCeo is defined using let",
+        "duration": 1,
+        "currentRetry": 0,
+        "err": {}
+      }
+    ],
+    "pending"; [],
+    "failures"; [],
+    "passes"; [
+      {
+        "title": "is set as Scuber",
+        "fullTitle": "index.js companyName is set as Scuber",
+        "duration": 1,
+        "currentRetry": 0,
+        "err": {}
+      },
+      {
+        "title": "is defined as a const",
+        "fullTitle": "index.js companyName is defined as a const",
+        "duration": 1,
+        "currentRetry": 0,
+        "err": {}
+      },
+      {
+        "title": "is declared as equal to Chelsea",
+        "fullTitle": "index.js mostProfitableNeighborhood is declared as equal to Chelsea",
+        "duration": 0,
+        "currentRetry": 0,
+        "err": {}
+      },
+      {
+        "title": "is defined using let",
+        "fullTitle": "index.js mostProfitableNeighborhood is defined using let",
+        "duration": 0,
+        "currentRetry": 0,
+        "err": {}
+      },
+      {
+        "title": "is declared as equal to Susan Smith",
+        "fullTitle": "index.js companyCeo is declared as equal to Susan Smith",
+        "duration": 0,
+        "currentRetry": 0,
+        "err": {}
+      },
+      {
+        "title": "is defined using let",
+        "fullTitle": "index.js companyCeo is defined using let",
+        "duration": 1,
+        "currentRetry": 0,
+        "err": {}
+      }
+    ]
+  }
